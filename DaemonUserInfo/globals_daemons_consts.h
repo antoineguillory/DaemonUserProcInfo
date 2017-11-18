02e65569f58eab3eca0b7987296289b97556485e
Antoine Guillory <antoineguillory@hotmail.fr>
2017-11-18 17:23:06 +0100

    Some Constants & server file

---
+++ DaemonUserInfo/globals_daemons_consts.h
@@ -0,0 +1,5 @@
+#ifndef GLOBALS_DAEMON_CONSTS
+#define GLOBALS_DAEMON_CONSTS
+
+#define FIFO_RQST_NAME "/tmp/Daemon_User_Info_58062572.fifo"
+#endif
---
+++ DaemonUserInfo/server.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <mman.h>
+
+#include "globals_daemons_consts.h"
+#include "server_consts.h"
+
+int main(void){
+
+    printf("Welcome to DaemonUserInfo (Version : %s)\n", SERVER_VERSION);
+    //TODO
+}
